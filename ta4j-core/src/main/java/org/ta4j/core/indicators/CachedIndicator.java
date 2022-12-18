e0a5275b5a05a727326cc564cd1a08b84e7dc62c
David <cookiefactory@gmail.com>
2022-12-18 09:10:12 -0500

    added fix for array OOB exception

--- ta4j-core/src/main/java/org/ta4j/core/indicators/CachedIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/CachedIndicator.java
@@ -127,17 +127,17 @@ public abstract class CachedIndicator<T> extends AbstractIndicator<T> {
                     int resultInnerIndex = results.size() - 1 - (highestResultIndex - index);
                     
                     if (resultInnerIndex < 0) {
-                        log.warn("resultInnerIndex calculated from highestResultIndex {} is {} and is out of range of results cache limit {}. Setting resultInnerIndex to 0", highestResultIndex, resultInnerIndex, results.size());
+                        log.debug("resultInnerIndex calculated from highestResultIndex {} is {} and is out of range of results cache limit {}. Setting resultInnerIndex to 0", highestResultIndex, resultInnerIndex, results.size());
                         resultInnerIndex = 0;
                     } else if (resultInnerIndex > results.size() - 1) {
-                        log.warn("resultInnerIndex calculated from highestResultIndex {} is {} and is out of range of results cache limit {}. Setting resultInnerIndex to {}", highestResultIndex, resultInnerIndex, results.size(), results.size() - 1);
+                        log.debug("resultInnerIndex calculated from highestResultIndex {} is {} and is out of range of results cache limit {}. Setting resultInnerIndex to {}", highestResultIndex, resultInnerIndex, results.size(), results.size() - 1);
                         resultInnerIndex = results.size() - 1;
                     }
                     
                     result = results.get(resultInnerIndex);
                     if (result == null) {
                         result = calculate(index);
-                        results.set(resultInnerIndex, result);
+                        results.set(Math.min(resultInnerIndex, results.size() - 1), result);
                     }
                 }
             }
