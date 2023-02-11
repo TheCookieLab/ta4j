3ed1eba58f0595ba577e7da71704576916a6f922
David <cookiefactory@gmail.com>
2023-02-11 14:29:51 -0500

    added flip flop rule

---
+++ ta4j-core/src/test/java/org/ta4j/core/rules/FlipFlopRuleTest.java
@@ -0,0 +1,59 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2022 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.rules;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FlipFlopRuleTest {
+
+    @Test
+    public void isSatisfiedStartingWithTrue() {
+        FlipFlopRule flipFlopRule = new FlipFlopRule();
+        assertTrue(flipFlopRule.isSatisfied(0));
+        assertFalse(flipFlopRule.isSatisfied(1));
+        assertTrue(flipFlopRule.isSatisfied(2));
+        assertFalse(flipFlopRule.isSatisfied(9));
+        assertTrue(flipFlopRule.isSatisfied(9));
+    }
+
+    @Test
+    public void isSatisfiedStartingWithFalse() {
+        FlipFlopRule flipFlopRule = new FlipFlopRule(false);
+        assertFalse(flipFlopRule.isSatisfied(0));
+        assertTrue(flipFlopRule.isSatisfied(1));
+        assertFalse(flipFlopRule.isSatisfied(2));
+        assertTrue(flipFlopRule.isSatisfied(3));
+        assertFalse(flipFlopRule.isSatisfied(4));
+        assertTrue(flipFlopRule.isSatisfied(5));
+        assertFalse(flipFlopRule.isSatisfied(4));
+        assertTrue(flipFlopRule.isSatisfied(3));
+        assertFalse(flipFlopRule.isSatisfied(2));
+        assertTrue(flipFlopRule.isSatisfied(1));
+        assertFalse(flipFlopRule.isSatisfied(0));
+
+    }
+}
